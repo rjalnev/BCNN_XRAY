b677de707fd3affe81041a36b228d52c87e854f2
rjalnev <RobertJamesEstes@gmail.com>
2020-11-15 18:26:07 -0600

    Added in SMOTE oversampling.

--- preprocess_data.py
+++ preprocess_data.py
@@ -5,9 +5,9 @@ from glob import glob #for parsing paths
 from skimage import io #for loading images
 from skimage.transform import resize #for downsizing
 from skimage.color import rgb2gray #for converting to grayscale
-from imblearn.over_sampling import RandomOverSampler #for class balance
+from imblearn.over_sampling import RandomOverSampler, SMOTE #for class balance
 
-from utils import plotClassDist, plotSamples, saveData
+from utils import plotClassDist, plotSamples, saveData, loadData
 
 def load_images(downsize = True, downsize_shape = (256, 256)):
     '''Load the images, downsize if true, convert to greyscale, combine and generate labels.'''
@@ -60,23 +60,40 @@ def randomOversample(data, labels):
     data, labels = ros.fit_resample(data, labels) #oversample
     data = data.reshape(data.shape[0], shp[1], shp[2]) #de-flatten data
     return data, labels
+    
+def smoteOversample(data, labels):
+    ''' Oversample the data using SMOTE. We need to flatten the images first and the deflatten when done.'''
+    shp = data.shape
+    data = data.reshape(shp[0], shp[1] * shp[2]) #flatten data
+    smt = SMOTE(random_state=42)
+    data, labels = smt.fit_resample(data, labels) #oversample using smote
+    data = data.reshape(data.shape[0], shp[1], shp[2]) #de-flatten data
+    return data, labels
 
 def main():
-    #load the images
-    data, labels = load_images()
+    #load the JPEG images or the npz file
+    #data, labels = load_images() #un-comment if want to load JPEG images into numpy
+    data, labels = loadData('data/data.npz')  #load images from npz
     
     #check imbalance
-    print('Data Shape:', data.shape, 'Labels Shape', labels.shape)
+    print('Data Shape:', data.shape, 'Labels Shape:', labels.shape)
     plotClassDist(labels)
     
-    #Random over sample and recheck balance
+    #random oversample and recheck balance
     ovs_data, ovs_labels = randomOversample(data, labels)
-    print('Data Shape:', ovs_data.shape, 'Labels Shape', ovs_labels.shape)
+    print('OVS Data Shape:', ovs_data.shape, 'OVS Labels Shape:', ovs_labels.shape)
     plotClassDist(ovs_labels)
+    
+    #oversample using smote and recheck balance
+    smt_data, smt_labels = smoteOversample(data, labels)
+    print('SMT Data Shape:', smt_data.shape, 'SMT Labels Shape:', smt_labels.shape)
+    plotClassDist(smt_labels)
     
     #save untouched and oversampled data as npz
     saveData('data/data.npz', data, labels)
     saveData('data/ros_data.npz', ovs_data, ovs_labels)
+    saveData('data/smt_data.npz', smt_data, smt_labels)
+    
     
 if __name__ == '__main__':
     main()
