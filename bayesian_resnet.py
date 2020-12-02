47bc7e016e1db96fe181c178cc6a1f87f99f98c2
rjalnev <RobertJamesEstes@gmail.com>
2020-12-01 18:31:25 -0600

    Began implementing a Bayesian resnet class.

--- bayesian_resnet.py
+++ bayesian_resnet.py
@@ -1,1 +1,90 @@
-# gonna ty to replace conv2d layers in my resnet with conv2D flipout later in tf probs
+import numpy as np
+
+import tensorflow as tf
+import tensorflow_probability as tfp
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.layers import Input, ReLU, BatchNormalization, Add, AveragePooling2D, Flatten
+
+from utils import loadData, saveData, plotSamples, plotClassDist
+
+class BayesianResNet():
+    
+    def __init__(self, load = False, directory = './model/', input_shape = None, num_classes = None,
+                 num_filters = 32, kernel = 3, blocks = [1, 2, 2, 1]):
+        ''''''
+        if load:
+            self.model = load_model(directory + 'model.h5')
+            self.current_epoch = np.genfromtxt(directory + 'model_history.csv', delimiter = ',', skip_header = 1).shape[0]
+        else:
+            assert (input_shape != None or num_classes != None), 'The arguments input_shape and num_filters must be specified if not loading a model.'
+            self.model = self.build_model(input_shape, num_classes, num_filters, kernel, blocks);
+            self.current_epoch = 0
+    
+    def build_model(self, input_shape, num_classes, num_filters, kernel, blocks):
+        ''''''
+        def residual_block(x, num_filters, kernel, stride, index):
+            out = tfp.layers.Convolution2DFlipout(num_filters, kernel, stride, padding = 'same', name = 'C{}'.format(index))(x)
+            index += 1
+            out = ReLU(name = 'RELU{}'.format(index-1))(out)
+            out = BatchNormalization(name = 'BN{}'.format(index-1))(out)
+            out = tfp.layers.Convolution2DFlipout(num_filters, kernel, 1, padding = 'same', name = 'C{}'.format(index))(out)
+            index += 1
+            if stride == 2: #Need to downsample input by have to match dimensions
+                x = tfp.layers.Convolution2DFlipout(num_filters, 1, stride, padding = 'same', name = 'C_DS{}'.format(index-3))(x)
+            out = Add(name = 'ADD{}'.format(index-3))([x, out])
+            out = ReLU(name = 'RELU{}'.format(index-1))(out)
+            out = BatchNormalization(name = 'BN{}'.format(index-1))(out)
+            return out, index
+
+        x = Input(shape = input_shape, name = 'input')
+        
+        out = tfp.layers.Convolution2DFlipout(num_filters, kernel, 1, padding = 'same', name = 'C1')(x)
+        out = ReLU(name = 'RELU1')(out)
+        out = BatchNormalization(name = 'BN1')(out)
+        
+        index = 2
+        for i in range(len(blocks)):
+            for j in range(blocks[i]):
+                if (i!=0 and j==0): stride = 2
+                else: stride = 1
+                out, index = residual_block(out, num_filters, kernel, stride, index)
+            num_filters *= 2
+                
+        out = AveragePooling2D(name = 'avgPool')(out)
+        out = Flatten(name = 'flatten')(out)
+        out = tfp.layers.DenseFlipout(num_classes, activation='softmax', name = 'output')(out)
+        
+        model = Model(x, out, name = 'resnet')
+        model.summary()
+        return model
+    
+    def fit(self, X, Y, validation_data = None, epochs = 10, batch_size = 32, optimizer = 'adam',
+            save = False, directory = './model/'):
+        ''''''
+        if save: # set callback functions if saving model
+            if not os.path.exists(directory): os.makedirs(directory)
+            mpath = directory + "wifinet.h5"
+            hpath = directory + 'wifinet_history.csv'
+            if validation_data is None:
+                checkpoint = ModelCheckpoint(filepath = mpath, monitor = 'loss', verbose = 0, save_best_only = True)
+            else:
+                checkpoint = ModelCheckpoint(filepath = mpath, monitor = 'val_loss', verbose = 0, save_best_only = True)
+            cvs_logger = CSVLogger(hpath, separator = ',', append = True)
+            callbacks = [cvs_logger, checkpoint]
+        else:
+            callbacks = None
+        
+        self.model.compile(optimizer, 'categorical_crossentropy', ['accuracy'],
+                           experimental_run_tf_function = False) #compile model
+        self.model.fit( X, Y, validation_data = validation_data,
+                        epochs = epochs, batch_size = batch_size, shuffle = True,
+                        initial_epoch = self.current_epoch,
+                        callbacks = callbacks,
+                        verbose = 1 ) #fit model
+                        
+        self.current_epoch += epochs
+        
+    def predict(self, x):
+        ''''''
+        return self.model.predict(x)
+        
--- main.py
+++ main.py
@@ -8,20 +8,24 @@ from tensorflow.keras.utils import to_categorical #use for one-hot encoding labe
 from tensorflow.keras.optimizers import Adam #optimizer
 
 from resnet import ResNet
+from bayesian_resnet import BayesianResNet
 from utils import loadData
 
 def main():
     ''''''
+    print('start')
     #load data from npz
     data, labels = loadData('data/ros_data.npz')
 
     #resnet with 4 stages with [1, 2, 2, 1] number of residual blocks
-    resnet = ResNet(input_shape = (256, 256, 1), num_classes = 2) #init model
-    opt = Adam(learning_rate = 1e-3, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-07, amsgrad = False) #setup optimizer
-    resnet.fit(np.expand_dims(data, axis = -1), to_categorical(labels), batch_size = 5, optimizer = opt, save=False) #train model
+    #resnet = ResNet(input_shape = (256, 256, 1), num_classes = 2) #init model
+    #opt1 = Adam(learning_rate = 1e-3, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-07, amsgrad = False) #setup optimizer
+    #resnet.fit(np.expand_dims(data, axis = -1), to_categorical(labels), batch_size = 5, optimizer = opt1, save=False) #train model
     
     #bayesian resnet
-    
+    bayesian_resnet = BayesianResNet(input_shape = (256, 256, 1), num_classes = 2) #init model
+    opt2 = Adam(learning_rate = 1e-3, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-07, amsgrad = False) #setup optimizer
+    bayesian_resnet.fit(np.expand_dims(data, axis = -1), to_categorical(labels), batch_size = 5, optimizer = opt2, save=False) #train model
     
 if __name__ == '__main__':
     main()
